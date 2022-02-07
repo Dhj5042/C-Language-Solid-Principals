06653cc9099ad2e285470918847f4e0b84719f7d
SF-CPU-312\dhruvin <dhruvin.j@simformsolutions.com>
2022-02-07 18:28:30 +0530

    Second Commit

---
+++ P2/Practical_2/Practical_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practical_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object initialize and Constructor called automatically
+            Customer_Account customer_Account = new Customer_Account("Dhruvin",5042535);
+            //assign customer bank name
+            customer_Account.bank_name = "Bank Of Baroda";
+            //called print method for display customer data
+            customer_Account.printInfo();
+        }
+    }
+}
---
+++ P3/Pratical_3_Abstraction/Pratical_3_Abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pratical_3_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialized object
+            Laptop laptop = new Laptop();
+            //assign private variables value by setter
+            laptop.Brand = "Acer";
+            laptop.Model = "Nitro 5";
+            //print following output
+            laptop.LaptopDetails();
+        }
+    }
+}
